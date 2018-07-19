74a2f9693c9a928770fefeae223106b772018732
Ryuya Ikeda <rikeda71@gmail.com>
2018-07-19 14:40:19 +0900

    Add: form and plot html render

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models.model.logistic import Logistic
 app = Flask(__name__)
+m = Logistic()
+m.load_model(fname="sentplot.npy")
 
 
 @app.route("/")
@@ -7,10 +10,32 @@ def hello_world():
     return "Hello World!"
 
 
-@app.route("/index")
-def index():
-    return render_template("index.html", message="test")
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+
+@app.route("/sentplot", methods=["POST"])
+def sentplot():
+    if request.method == "POST":
+        sentence = request.form["sentence"]
+    result = m.predict(sentence)
+    sign, per, vec = result
+    vec = list(vec)
+    w = list(m.w)
+    return render_template("sentplot.html", w=w, sign=sign, per=per, vec=vec)
+
+
+def return_sample_vecs():
+    with open("models/dataset/concat.csv") as f:
+        lines = [line.replace("\n", "")[2:] for line in f.readlines()]
+    test = lines[-50:]
+    dic = {-1: {}, 1: {}}
+    for t in test:
+        p = m.predict(t)
+        dic[p[0]][t] = p[2]
+    return dic
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="1.0.0.0", debug=True)
