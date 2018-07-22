b8fcf2f2947cc725e87fc45ce52749aabccb9b41
Ryuya Ikeda <rikeda71@gmail.com>
2018-07-22 22:38:38 +0900

    Add: 分離平面の可視化 close #2

--- static/test.js
+++ static/test.js
@@ -1,10 +1,49 @@
-function plotclassifier(sepline, vectors){
-  vecs = arrange_vec();
-  vecs.unshift(
-    {
+function plotclassifier(sepline, vector){
+  var [vecs, xmin, xmax, ymin, ymax]
+      = arrange_vec(-sepline[0] / sepline[1], vector);
+  Highcharts.chart('container', {
+    xAxis: {
+      title: {
+        enabled: true,
+        text: 'positive condition'
+      },
+      min: xmin,
+      max: xmax
+    },
+    yAxis: {
+      title: {
+        enabled: true,
+        text: 'negative condition'
+      },
+      min: ymin,
+      max: ymax
+    },
+    title: {
+      text: 'Separation line and vectors of sentence in vector space'
+    },
+    tooltip: {
+      pointFormat: 'text: {point.text}<br>x: {point.x}<br>y: {point.y}',
+    },
+    series: vecs
+  });
+}
+
+function arrange_vec(slope, input){
+  $.ajaxSetup({ async: false });
+  var vecs = [];
+  var xmin, xmax, ymin, ymax;
+  $.getJSON("static/test.json", function(json){
+    xmin = Math.min.apply(null, json.vecs.x);
+    xmax = Math.max.apply(null, json.vecs.x);
+    ymin = xmin * slope;
+    ymax = xmax * slope;
+    /* 分離平面の設定 */
+    const sepline = {
       type: 'line',
-      name: 'Regression Line',
-      data: [[0, 1.11], [5, 4.51]],
+      name: 'Separation line',
+      // ベクトルの最大値，最小値を活用
+      data: [[xmin, ymin],
+             [xmax, ymax]],
       marker: {
         enabled: false
       },
@@ -15,42 +54,28 @@ function plotclassifier(sepline, vectors){
       },
       enableMouseTracking: false
     }
-  )
-  Highcharts.chart('container', {
-    xAxis: {
-      title: {
-        enabled: true,
-        text: 'positive condition'
+    /* 点ベクトルのフォーマット */
+    // 入力テキストのベクトル
+    var inputvec = {
+      type: 'scatter',
+      name: 'input sentence vector',
+      data: [{
+        x: input[0],
+        y: input[1],
+        text: 'sentence'
+      }],
+      color: 'rgba(80, 230, 80, 1)',
+      marker: {
+        radius: 6,
       },
-      min: -0.5,
-      max: 5.5
-    },
-    yAxis: {
-      title: {
-        enabled: true,
-        text: 'negative condition'
-      },
-      min: 0
-    },
-    title: {
-      text: 'Scatter plot with regression line'
-    },
-    tooltip: {
-      pointFormat: 'text: {point.text}<br>x: {point.x}<br>y: {point.y}',
-    },
-    series: vecs
-  });
-}
 
-function arrange_vec(){
-  $.ajaxSetup({ async: false });
-  var vecs = []
-  $.getJSON("static/test.json", function(json){
+    }
+    // サンプルテキストのベクトル
     var pos = {
       type: 'scatter',
       name: 'positive',
       data: [],
-      color: 'rgba(223, 83, 83, .5)',
+      color: 'rgba(223, 83, 83, .8)',
       marker: {
         radius: 4,
       }
@@ -59,19 +84,20 @@ function arrange_vec(){
       type: 'scatter',
       name: 'negative',
       data: [],
-      color: 'rgba(119, 120, 191, .5)',
+      color: 'rgba(119, 120, 191, .8)',
       marker: {
         radius: 4,
       }
     }
-    const len = json.sentences.length;
     var obj
-    for (var i = 0; i < len; ++i) {
+    // 点ベクトルをjsonから読み込んで用意
+    for (var i = 0; i < json.sentences.length; ++i) {
       obj = {
         x: json.vecs.x[i],
         y: json.vecs.y[i],
         text: json.sentences[i]
       }
+      // ネガポジで保存先を分岐
       if (json.sign[i] == 1) {
         pos.data.push(obj);
       }
@@ -80,8 +106,11 @@ function arrange_vec(){
       }
     }
     $.ajaxSetup({ async: true });
+    // 可視化のパーツを1つずつ格納
+    vecs.push(sepline);
+    vecs.push(inputvec);
     vecs.push(pos);
     vecs.push(neg);
   });
-  return vecs
+  return [vecs, xmin, xmax, ymin, ymax];
 }
